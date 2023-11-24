249f8d9652a68831b5f3ad959bb656b786526c0a
akffntm12 <akffntm12@naver.com>
2023-11-24 12:15:28 +0900

    feat: jpa 설정 제거

--- src/main/java/com/example/kakao/sample/SampleDataRepository.java
+++ src/main/java/com/example/kakao/sample/SampleDataRepository.java
@@ -1,8 +1,6 @@
 package com.example.kakao.sample;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
-public interface SampleDataRepository extends JpaRepository<SampleData, Integer> {
+public interface SampleDataRepository {
 }
